992188aa3d388d6aadc550ae2088065f8bca4ecc
felipeuntill <felipeassuncao.net@gmail.com>
2017-05-24 18:42:14 -0300

    draft sync

---
+++ cli/commands/sync.js
@@ -0,0 +1,9 @@
+const events = require('../../src/data/events.json');
+
+class list {
+   static execute(){
+      console.log('Sincronizando os eventos com o ambiente de produção');
+   }
+ }
+
+ module.exports = list;
