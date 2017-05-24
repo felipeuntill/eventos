5dc7fb90464e51cf389d9c5e8651dff0995c6760
felipeuntill <felipeassuncao.net@gmail.com>
2017-05-24 18:39:26 -0300

    adicionando o colors just pra testar

--- cli/printer/eventPrinter.js
+++ cli/printer/eventPrinter.js
@@ -1,4 +1,6 @@
+const colors = require('colors');
 const printer = require('./printer.js');
+
 
 const months = {
   Janeiro : '01',
@@ -20,7 +22,7 @@ class eventPrinter extends printer {
     var eventDate = new Date(event.date.year, months[event.date.month], event.date.day);
     var expired = eventDate < Date.now();
 
-    this.write(`${event.title} - ${expired ? "Expirado" : `${event.date.day}/${months[event.date.month]}/${event.date.year}`}`);
+    this.write(`${event.title} - ${expired ? "Expirado".red : `${event.date.day}/${months[event.date.month]}/${event.date.year}`.magenta}`);
   }
 }
 
