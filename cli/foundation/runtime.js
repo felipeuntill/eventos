e0b41049462f43eead6ac9a722ccf1b178249a09
felipeuntill <felipeassuncao.net@gmail.com>
2017-05-24 19:16:52 -0300

    agora a cli também suporta newba

--- cli/foundation/runtime.js
+++ cli/foundation/runtime.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const config = require('../config.json');
 
 class runtime {
@@ -22,11 +23,51 @@ class runtime {
   }
 
   static executeOptions(program){
+
+    let anyExecuted = false;
+
     this.iterateOptions((option, command)=> {
       if(!program[option]) return;
-      var func =  require(`../commands/${option}.js`);
-      func[command.method]();
+      this.executeOption(option);
+      anyExecuted = true;
     });
+
+    // Se o cara não passar porra nenhuma trata ele como criança e manda as tasks.
+    if(!anyExecuted)
+      this.displayOptions(program);
+
+  }
+
+
+
+  static displayOptions (program){
+
+    var choices = [];
+
+    this.iterateOptions((option, command)=> {
+      choices.push({
+        key: command.argument,
+        name: `--${option}, ${command.description}`,
+        value: option
+      })
+    });
+
+    var questions = [{
+      type: 'expand',
+      name: 'command',
+      message: 'Qual dos commandos você pretende executar?',
+      choices: choices
+    }];
+
+    inquirer.prompt(questions).then(function (answer) {
+      runtime.executeOption(answer.command);
+    });
+
+  }
+
+  static executeOption(option){
+    var func =  require(`../commands/${option}.js`);
+    func.execute();
   }
 
   static parseArguments(program){
@@ -36,6 +77,7 @@ class runtime {
   static iterateOptions (callback) {
     for (var option in config.options){
       if (config.options.hasOwnProperty(option)) {
+
         const command = config.options[option];
         callback(option, command)
       }
