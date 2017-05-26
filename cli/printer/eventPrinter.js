eee2cad7422498047921079fbb75d67c38b18992
felipeuntill <felipeassuncao.net@gmail.com>
2017-05-26 17:25:30 -0300

    fix exibição expired

--- cli/printer/eventPrinter.js
+++ cli/printer/eventPrinter.js
@@ -19,10 +19,22 @@ const months = {
 
 class eventPrinter extends printer {
   static print (event){
-    var eventDate = new Date(event.date.year, months[event.date.month], event.date.day);
-    var expired = eventDate < Date.now();
 
-    this.write(`${event.title} - ${expired ? "Expirado".red : `${event.date.day}/${months[event.date.month]}/${event.date.year}`.magenta}`);
+    let multiplesDays = event.date.day.split('e').length > 1;
+    let eventDay = event.date.day;
+
+    if(multiplesDays)
+      eventDay = parseInt(event.date.day.split('e')[1])
+
+    let eventDate = new Date(event.date.year,(parseInt(months[event.date.month])-1), eventDay);
+    let currentdate = new Date();
+    let nowDate = new Date(currentdate.getFullYear(), (currentdate.getMonth()), currentdate.getDate());
+    let expired = eventDate < nowDate;
+
+    console.log('-------------------------------------------------------------------------------------')
+    console.log(eventDate);
+    console.log(nowDate);
+    this.write(`${event.title} - ${expired ? `${event.date.day}/${months[event.date.month]}/${event.date.year} (Expirado)`.red : `${event.date.day}/${months[event.date.month]}/${event.date.year}`.magenta}`);
   }
 }
 
