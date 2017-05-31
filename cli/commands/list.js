70db6a245859b66273cb84e4b9757acca6714456
felipeuntill <felipeassuncao.net@gmail.com>
2017-05-31 18:45:50 -0300

    quase ok

--- cli/commands/list.js
+++ cli/commands/list.js
@@ -1,16 +1,25 @@
-const events = require('../../src/data/events.json');
+const uploader = require('../foundation/uploader.js');
 const eventPrinter = require('../printer/eventPrinter.js');
 
 class list {
    static execute(){
-      console.log('Listando eventos cadastrados');
+      console.log('Listando eventos existêntes no ambiente de produção');
       this.iterateEvents();
    }
 
    static iterateEvents(){
-     events.forEach((event) => {
-       eventPrinter.print(event);
-     })
+
+     const helper = new uploader();
+
+     helper.get('data/events.json', (file) => {
+
+       const events = JSON.parse(file.content);
+
+       events.forEach((event) => {
+         eventPrinter.print(event);
+       })
+
+     });
    }
  }
 
--- cli/commands/sync.js
+++ cli/commands/sync.js
@@ -1,19 +1,23 @@
-const events = require('../../src/data/events.json');
 const uploader = require('../foundation/uploader.js');
+const eventPrinter = require('../printer/eventPrinter.js');
 
 class sync {
    static execute(){
+
       console.log('Sincronizando os eventos com o ambiente de produção');
 
-      var helper = new uploader();
+      const helper = new uploader();
 
       helper.get('data/events.json', (file) => {
-        console.log(file);
-      });
 
+        const events = JSON.parse(file.content);
+
+        events.forEach((event) => {
+          eventPrinter.print(event);
+        })
+
+      });
    }
  }
-
-sync.execute();
 
  module.exports = sync;
